3bf95791e0807ecc4248639b7a1823faac2aa7bb
Marko Manninen <elonmedia@gmail.com>
2023-08-08 11:28:48 +0300

    Update tocmanuscript.py
    Comment additions, docs and minor functionality changes

--- tocmanuscript.py
+++ tocmanuscript.py
@@ -5,52 +5,54 @@ Manuscript Content Generation and Management in ChatGPT using Noteable Plugin an
 
 Copyright © 08/2023
 
-Unlock the power of structured content creation with the ToCManuscript module in ChatGPT, seamlessly integrated with the Noteable plugin. This tool allows you to define, manage, and generate manuscript content through a hierarchical Table of Contents (TOC), all within an interactive chat environment.
+Unlock the power of structured content creation with the ToCManuscript module in ChatGPT, integrated with the Noteable plugin. This tool allows you to define, manage, and generate manuscript content through a hierarchical Table of Contents (TOC), all within an interactive chat environment.
 
 With ToCManuscript, you can:
 
 - **Define a Hierarchical TOC**: Outline your manuscript with headings, subheadings, and descriptions in a nested tree format.
 - **Set Prompts and Guidelines**: Customize the content generation process with specific prompts, guidelines, and constraints for each section.
 - **Generate Content Iteratively**: Let ChatGPT create content for each section, one by one, guided by the prompts you've set.
-- **Manage Content Progress**: Track the completion status, edit drafts, and navigate through the TOC with ease.
+- **Manage Content Progress**: Track the completion status, edit drafts, and navigate through the TOC.
 - **Export to Markdown**: Save the completed manuscript to a markdown file, ready for further editing or publishing.
 
-Whether you're working on a research paper, a novel, a technical manual, or any other long-form content, ToCManuscript streamlines the process, making it more creative, productive, and enjoyable.
+Whether you're working on a research paper, a novel, a technical manual, or any other long-form content, ToCManuscript streamlines the process, making it more creative and experimental.
 
-Get started with ToCManuscript and transform the way you write!
+Copy and paste the following wizard prompt to the ChatGPT's text input with Noteable plugin activated.
+
 
 ## START WIZARD
 
-This step-by-step procedure will help user to generate manuscript content by using ChatGPT and the Noteable plugin. This procedure will use custom classes, initialize them, ask TOC titles and prompts, generate content, and save the completed manuscript to the `.md` text file.
+This step-by-step procedure will help user to generate manuscript content by using ChatGPT and the Noteable plugin. This procedure will download tocmanuscript module, import necessary classes, get documentation, initialize instances of the classes, ask TOC titles and prompts, generate content iteratively section by section, and save the completed manuscript to the `.md` text file.
 
-All steps are stored in the notebook cells for running them independently at any time. The associated ChatGPT conversation can be shared to show how it was used to generate the manuscript.
+Steps are stored in the notebook cells for running them independently at any time. The associated ChatGPT conversation can be shared (for paid v4 users) to show how it was used to generate the manuscript.
 
-Download the module from GitHub if it is not available in the Noteable project or if the user wants to replace the file with appended option: `-O tocmanuscript.py`:
+a) Download the module from GitHub if it is not available in the Noteable project or if the user wants to replace the file with appended option: `-O tocmanuscript.py`:
 
 ```
 !wget https://raw.githubusercontent.com/markomanninen/tocmanuscript/main/tocmanuscript.py
 ```
 
-a) Start a new Noteable notebook (or use a given one) to import classes:
+b) Start a new Noteable notebook (or use a given one) to import classes:
 
 ```
 from tocmanuscript import ToCDict, ToCManuscript, Prompt, Author, docs
 ```
 
-b) Read ToCManuscript, Prompt, Author, and ToCDict class documentation for later reference:
+c) Read ToCManuscript, Prompt, Author, and ToCDict class documentation for later reference:
 
 ```
-docs(ToCManuscript, Prompt, Author, ToCDict)
+docs(ToCManuscript, Author)
 ```
 
-c) Ask manuscript title and author information from the user.
+d) Ask manuscript title and author information from the user.
 
-d) Init author and toc_manuscript instances with the given information:
+e) Init author and toc_manuscript instances with the given information:
 
 ```
 author = Author("John Doe")
 toc_manuscript = ToCManuscript(title="Manuscript title", author=author)
 ```
+
 
 ## STEP 1
 
@@ -66,62 +68,77 @@ Heading 1
 
 Ask the user to provide TOC or help the user to create one.
 
+Store toc as string in a variable so that we can refer to it at any time in the future: `toc_text = '...'`
+
+
 ## STEP 2
 
-Set general guidelines for LLM prompts:
+a) Read prompt class documentation to guide throught this step: `docs(Prompt)`
+
+b) Set general guidelines for LLM prompts:
 
 ```
 guidelines={'Role': '', 'Style': '', 'Format': '', 'Context': '', ...}
+restrictions={'Content': 'emit main heading in the beginning and conclusive part at the end of the text'}
 ```
 
-This can be used as a global system prompt for LLM. But it can be overridden at any specific section prompt.
+These can be used kind of a global system prompt for LLM. But they can be overridden at any specific section prompt.
+
 
 ## STEP 3 (iterative)
 
-a) Generate prompts for each section, one by one, to guide the future content creation in step 4:
+These definitions will guide the future content creation in step 4.
+
+a) Generate prompts for each section:
 
 ```
 section_1_prompt = Prompt(
     # Based on the current section title
-    directive={'Instruction': '...'},
+    directives={'Instruction': '...'},
     # Use general guidelines or extend it
     guidelines=guidelines,
-    # Optionally use restrictive definitions for prompt
-    restrictions={...}
+    # Restrictive definitions for prompt
+    restrictions=restrictions
 )
 ```
 
-See more help: `print(Prompt.__init__.__doc__)`
+See more help about different prompt parameter options and examples: `print(Prompt.__init__.__doc__)`
 
 b) Create a title and prompt entry for the section. The value must be `ToCDict` type:
 
 ```
-toc_manuscript[1] = ToCDict({'title': 'Heading level one', prompt: section_1_prompt})
+toc_manuscript[1] = ToCDict({'title': 'Heading level one', 'prompt': section_1_prompt})
 ```
 
-You may initialize a group of sections, like one chapter in one cell, but possibly half a dozen sections at most because the generated text will be limited by length. Proceeding with smaller tasks is best.
+You may initialize a group of sections, like one chapter in one cell, but possibly half a dozen sections at most because the generated text will be limited by length. Proceeding with smaller tasks is best. Note: if `prompt` is not given, then the `content` is not supposed to follow the `title`, and `completed` parameter will automatically be `True`.
+
+For example:
 
 ```
-toc_manuscript[1][1] = ToCDict({'title': 'Heading level two', prompt: section_1_1_prompt})
+toc_manuscript[1] = ToCDict({'title': 'Heading level one', 'prompt': section_1_prompt})
+toc_manuscript[1][1] = ToCDict({'title': 'Heading level two', 'prompt': section_1_1_prompt})
+toc_manuscript[2] = ToCDict({'title': 'Only heading level one', 'completed'=True})
 ```
 
-Repeat step 3. Ask the user for permission to proceed to the next item(s).
+Repeat STEP 3.
 
-After the step 3 final iteration, you can call `toc_manuscript.print_toc()` to see the intended table of contents in plain text format.
 
 ## STEP 4 (iterative)
+
+After the the last item in STEP 3 iteration, you can call `toc_manuscript.print_toc()` to see the intended table of contents in plain text format.
 
 Once all titles and prompts are set, let Noteable + ChatGPT generate a content cell for each item in TOC, one by one.
 
 a) Call `next_index = toc_manuscript.move_to_next_section()` to shift currently_editing_index to the next section.
 
-b) Get prompt data: `next_prompt = toc_manuscript.get_currently_editing_prompt()`
+b) Get prompt data and print them:
 
 ```
+next_prompt = toc_manuscript.get_currently_editing_prompt()
 print(next_index, next_prompt)
 ```
 
-Wait for a user's permission to proceed to the next steps c) and d).
+Wait for a user's permission to proceed to the next phases c) and d). It is important to stop at this phase and let LLM read the output of the last print. It will guide LLM to generate sophisticated content.
 
 c) Let LLM generate content for the variable:
 
@@ -129,13 +146,14 @@ c) Let LLM generate content for the variable:
 content = "..."
 ```
 
-d) Set content and completion state (`True`|`False`), `True` means complete, `False` means draft:
+d) Set content and completion state (`True`|`False`), `True` means complete, `False` means draft, which is the default value:
 
 ```
-toc_manuscript.set_currently_editing_content(content, completed=True)
+toc_manuscript.set_currently_editing_content(content, completed=bool)
 ```
 
-Repeat step 4. Ask the user for permission to proceed to the next item(s).
+Repeat STEP 4. Ask the user for permission to proceed to the next item(s).
+
 
 ## STEP 5
 
@@ -158,9 +176,25 @@ def docs(*args):
     """ Print doc string of the main classes. """
     for cls in args:
         print(cls)
-        print(cls.__setitem__.__doc__ if cls == ToCDict else cls.__doc__)
+        print(cls.__doc__)
 
 class ToCDict(dict):
+    """
+    A specialized dictionary class that extends the built-in dict type to manage a table of contents (ToC) structure.
+    The ToCDict class provides custom handling for setting items, ensuring that the values adhere to a specific structure
+    with attributes like 'prompt', 'title', 'created', 'modified', and 'completed'.
+
+    The __setitem__ method is overridden to enforce these constraints and manage timestamps for 'created' and 'modified',
+    as well as the 'completed' attribute.
+
+    Usage:
+        a = ToCDict({})
+        a[1] = ToCDict({'title': 'title 1'})
+        # This is the right way to setting new index / subsection.
+        a[1][1] = ToCDict({'title': 'title 1.1'})
+        # This is the wrong way. Title 2.1 is not going to reach __setitem__.
+        a[2] = ToCDict({'title': 'title 2', 1: ToCDict({'title': 'title 2.1'})})
+    """
     def __setitem__(self, key, value):
         """
         ToCDict __setitem__ overrides the default behavior for setting an item's value. Ensures that the value is a dictionary and contains specific keys like 'prompt' and 'title'. Also manages timestamps for 'created' and 'modified' and sets the 'completed' attribute.
@@ -172,16 +206,9 @@ class ToCDict(dict):
             - 'modified': (Automatically set) The timestamp when the value was last modified.
             - 'created': (Automatically set if not provided) The timestamp when the value was created.
             - 'completed': (Automatically set to False if not provided) A flag indicating whether the task associated with the key is completed.
+                           If prompt has not been given, then title is printed without content and completed is automatically True.
 
         :raises ValueError: If the value is not a dictionary, if 'prompt' is not a Prompt object, or if 'title' is not given.
-
-        Example:
-            a = ToCDict({})
-            a[1] = ToCDict({'title': 'title 1'})
-            # This is the right way to setting new index / subsection.
-            a[1][1] = ToCDict({'title': 'title 1.1'})
-            # This is the wrong way. Title 2.1 is not going to reach __setitem__.
-            a[2] = ToCDict({'title': 'title 2', 1: ToCDict({'title': 'title 2.1'})})
         """
         if type(value) == ToCDict:
             if 'prompt' in value and type(value['prompt']) != Prompt:
@@ -194,6 +221,8 @@ class ToCDict(dict):
                 value['created'] = value['modified']
             if 'completed' not in value:
                 value['completed'] = False
+            if 'prompt' not in value:
+                value['completed'] = True
         super().__setitem__(key, value)
 
 class ToCManuscript(ToCDict):
@@ -211,7 +240,7 @@ class ToCManuscript(ToCDict):
         })
         toc_manuscript[1][1] = ToCDict({
             'title': 'Heading level two',
-            'prompt': Prompt(directive={'Question': 'How to write a best-seller?'}),
+            'prompt': Prompt(directives={'Question': 'How to write a best-seller?'}),
         })
 
         # Iterate 2-3
@@ -580,7 +609,8 @@ class ToCManuscript(ToCDict):
                 if isinstance(value, ToCDict):
                      # Get the title and print with indentation
                     title = value.get('title', '')
-                    print(f'{indent}- {title}')
+                    completed = ' (completed)' if value.get('completed', False) else ''
+                    print(f'{indent}- {title}{completed}')
                     # Recursive call for nested levels, increasing the indentation
                     _print_toc(value, level + 1)
 
@@ -635,7 +665,30 @@ class Author(dict):
 
 class Prompt:
     """
-    The Prompt class is designed to encapsulate directives, guidelines, and constraints for guiding a large language model's response. Here's a summary of its functionality:
+    The Prompt class is designed to encapsulate directives, guidelines, and constraints for guiding a large language model's response. Consider the following writing tips:
+
+    1. Target Audience, 2. Message and Theme, 3. Purpose, 4. Style and Tone, 5. Structuring and Organization, 6. Main Characters/Concepts, 7. Evidence/Bakcground Research, and
+
+    8. Textual Devices and Language Usage:
+
+        Fiction:
+            Metaphors and Similes: Creating vivid imagery.
+            Dialogue: Revealing character personality.
+            Foreshadowing: Building suspense.
+            Alliteration and Assonance: Enhancing rhythm.
+            Point of View: Controlling narration.
+        Non-Fiction:
+            Clarity and Precision: Ensuring understanding.
+            Data Visualization: Enhancing comprehension.
+            Rhetorical Questions: Engaging readers.
+            Anecdotes and Case Studies: Humanizing concepts.
+            Citation and Referencing: Establishing credibility.
+        Both:
+            Tone and Voice: Creating mood and connection.
+            Active vs. Passive Voice: Controlling focus.
+            Transitional Phrases: Ensuring smooth flow.
+
+    Here's a summary of the Prompt class parameters that encapsulates all the user-given definitions:
 
     Directives: These are the core instructions that guide the model's response. They can include various subcategories like questions, statements, instructions, creative requests, analytical questions, hypothetical scenarios, debates, dialogues, step-by-step, and more.
 
@@ -656,7 +709,7 @@ class Prompt:
 
         # Create a Prompt object for section 1
         section_1_prompt = Prompt(
-            directive={'Instruction': 'Write an introduction to the topic of artificial intelligence.'},
+            directives={'Instruction': 'Write an introduction to the topic of artificial intelligence.'},
             guidelines=guidelines,
             constraints=restrictions
         )
